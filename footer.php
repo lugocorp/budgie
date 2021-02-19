57b1ca977ace66427e78af7946d4165cc7faa163
alugocp@aim.com <alugocp@aim.com>
2021-02-19 17:42:17 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<div id="footer">
+  <div>
+    <div>
+      <a href="">GitHub</a>
+      <a href="">NPM</a>
+      <a href="">Install</a>
+    </div>
+    <div>
+      <a href="">Library</a>
+      <a href="">Tool</a>
+      <a href="">Examples</a>
+    </div>
+    <div>
+      <a href="">About</a>
+      <a href="">Donate</a>
+      <a href="">Contact</a>
+    </div>
+  </div>
+  <p>Brought to you by <a href="http://lugocorp.net">LugoCorp</a></p>
+</div>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>Budgie.js</title>
+  <link rel="stylesheet" href="./style.css"/>
+</head>
+<body>
+  <?php include("./navbar.php");?>
+
+  <!-- Splash -->
+  <div id="splash">
+    <p>Make arcade-style HTML5 games with Budgie!</p>
+    <div>
+      <div>
+        <img src="res/budgie.svg"/>
+      </div>
+      <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/igH6NGK7Jwo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+
+  <!-- About -->
+  <div id="about">
+
+  </div>
+
+  <!-- Features -->
+  <div id="features">
+
+  </div>
+
+  <!-- Contact -->
+  <div id="contact">
+
+  </div>
+
+  <!-- Donate -->
+  <div id="donate">
+
+  </div>
+
+  <?php
+    $DIR=".";
+    include("./footer.php");
+  ?>
+</body>
+</html>
